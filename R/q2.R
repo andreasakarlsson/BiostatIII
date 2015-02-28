e3324465f7db167dcb5c518b06b675dbb8909ad7
andreasakarlsson <andreas.a.karlsson@gmail.com>
2015-02-28 16:24:21 +0100

    q2: html presentation of q2

--- R/q2.R
+++ R/q2.R
@@ -25,10 +25,11 @@ require(dplyr)
 require(ggplot2)
 require(GGally)
 require(gridExtra)
+require(scales)
 
-## Call muhaz smoother by strata function
+## wrapper function for muhaz hazard smoother by strata
 smoothHazard <- function(df, strat){
-    tmp <- lapply(as.list(levels(df[,strat])),
+    tmp <- lapply(as.list(sort(levels(df[,strat]))),
                   function(x) c(strat=x,with(melanoma,muhaz(times=surv_mm, delta=death_cancer, subset=df[strat]==x))))
     out <- do.call("rbind",
                    lapply(tmp,function(obj) data.frame(Hazard=obj$haz.est, Time=obj$est.grid, Strata=obj$strat)))
@@ -37,123 +38,90 @@ smoothHazard <- function(df, strat){
 
 ## @knitr loadPreprocess
 melanoma_raw <- read.dta13("http://biostat3.net/download/melanoma.dta")
+head(melanoma_raw[c("age", "sex", "stage", "status", "surv_mm", "surv_yy")])
 melanoma <- mutate(melanoma_raw,
                    death_cancer = ifelse( status == "Dead: cancer", 1, 0),
                    death_all = ifelse( status == "Dead: cancer" | status == "Dead: other", 1, 0))
 
-
-## a Tabulate events by stage
 ## @knitr a_tabulate
 melanoma %>%
     group_by(stage) %>%
-    summarise(Freq = n(),
-              Percent = n()/dim(melanoma)[1]) %>%
+    summarise(Freq = n(), Percent = n()/dim(melanoma)[1]) %>%
     mutate(Cum = cumsum(Percent))
 
-## Fit and plot survival
 ## @knitr a_plotSurv
 mfit <- survfit(Surv(surv_mm, death_cancer==1)~stage, data = melanoma)
 p1 <- ggsurv(mfit, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates") +
-    theme(legend.position="bottom")
+    ggtitle("Kaplan-Meier survival estimates") + theme(legend.position="bottom")
 ## Fit and plot smoothed hazard
 p2 <- ggplot(smoothHazard(melanoma,"stage"), aes(x=Time, y=Hazard, colour= Strata)) +
-    geom_line() +
-    ggtitle("Smoothed Hazard estimates") +
-    theme(legend.position="bottom")
+    geom_line() + ggtitle("Smoothed Hazard estimates") + theme(legend.position="bottom")
 grid.arrange(p1, p2, ncol=2)
 
-
-## Tabulate crude rates by month and year
 ## @knitr b_crudeRates
 melanoma %>%
     select(death_cancer, surv_mm, stage) %>%
     group_by(stage) %>%
-    summarise(D = sum(death_cancer),
-              M = sum(surv_mm),
-              Rate = D/M) %>%
-    mutate(CI_lower = Rate + qnorm(0.025) * Rate / sqrt(D),
+    summarise(D = sum(death_cancer), M = sum(surv_mm), Rate = D/M) %>%
+    mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
 melanoma %>%
     select(death_cancer, surv_yy, stage) %>%
     group_by(stage) %>%
-    summarise(D = sum(death_cancer),
-              Y = sum(surv_yy),
-              Rate = D/Y) %>%
-    mutate(CI_lower = Rate + qnorm(0.025) * Rate / sqrt(D),
+    summarise(D = sum(death_cancer), Y = sum(surv_yy), Rate = D/Y) %>%
+    mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
-## Tabulate crude rates per 1000   
 ## @knitr c_crudeRates1000 
 melanoma %>%
     select(death_cancer, surv_yy, stage) %>%
     group_by(stage) %>%
-    summarise(D = sum(death_cancer),
-              Y = sum(surv_yy)/1000,
-              Rate = D/Y) %>%
-    mutate(CI_lower = Rate + qnorm(0.025) * Rate / sqrt(D),
+    summarise(D = sum(death_cancer), Y = sum(surv_yy)/1000, Rate = D/Y) %>%
+    mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
-## Tabulate crude rate and plot S(t) & Haz by sex
 ## @knitr d_crudeRates1000_sex
 melanoma %>%
     select(death_cancer, surv_yy, sex) %>%
     group_by(sex) %>%
-    summarise(D = sum(death_cancer),
-              Y = sum(surv_yy)/1000,
-              Rate = D/Y) %>%
-    mutate(CI_lower = Rate + qnorm(0.025) * Rate / sqrt(D),
+    summarise(D = sum(death_cancer), Y = sum(surv_yy)/1000, Rate = D/Y) %>%
+    mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
 ## @knitr d_plotSurv_sex
-## Fit and plot survival
 sfit <- survfit(Surv(surv_mm, death_cancer==1)~sex, data = melanoma)
 p3 <- ggsurv(sfit, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates") +
-    theme(legend.position="bottom")
+    ggtitle("Kaplan-Meier survival estimates") + theme(legend.position="bottom")
 #Kaplan-Meier hazards smoothed 
 p4 <- ggplot(smoothHazard(melanoma, "sex"), aes(x=Time, y=Hazard, colour= Strata)) +
-    geom_line() +
-    ggtitle("Smoothed Hazard estimates") +
-    theme(legend.position="bottom")
+    geom_line() + ggtitle("Smoothed Hazard estimates") + theme(legend.position="bottom")
 grid.arrange(p3, p4, ncol=2)
 
-
-##e Tabulate by age group
 ## @knitr e_tabByAge
 with(melanoma,table(status, agegrp))
 
-
-##f Kaplan-Meier estimate Cancer vs All-cause
 ## @knitr f_survStage
 afit <- survfit(Surv(surv_mm, death_all==1)~stage, data = melanoma)
-ggsurv(afit, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates\nAll-cause") +
-    theme(legend.position="bottom")
+ggsurv(afit, cens.shape="|", cens.col = "black") + theme(legend.position="bottom") +
+    ggtitle("Kaplan-Meier survival estimates\nAll-cause")
 
-##g Kaplan-Meier estimate Cancer vs All-cause | age 75+
 ## @knitr g_allCa75p
 mfit75 <- survfit(Surv(surv_mm, death_cancer==1)~stage, data = subset(melanoma,agegrp=="75+"))
 afit75 <- survfit(Surv(surv_mm, death_all==1)~stage, data = subset(melanoma,agegrp=="75+"))
-p6 <- ggsurv(mfit75, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates\nCancer | Age 75+") +
-    theme(legend.position="bottom")
-p7 <- ggsurv(afit75, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates\nAll-cause | Age 75+") +
-    theme(legend.position="bottom")
+p6 <- ggsurv(mfit75, cens.shape="|", cens.col = "black") + theme(legend.position="bottom") +
+    ggtitle("Kaplan-Meier survival estimates\nCancer | Age 75+")
+p7 <- ggsurv(afit75, cens.shape="|", cens.col = "black") + theme(legend.position="bottom") +
+    ggtitle("Kaplan-Meier survival estimates\nAll-cause | Age 75+")
 grid.arrange(p6, p7, ncol=2)
 
-## Kaplan-Meier estimate Cancer vs All-cause by age group
 ## @knitr h_allCaAgeGrp 
 mfitage <- survfit(Surv(surv_mm, death_cancer==1)~agegrp, data = melanoma)
 afitage <- survfit(Surv(surv_mm, death_all==1)~agegrp, data = melanoma)
-p8 <- ggsurv(mfitage, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier estimates of\ncancer survival by age group") +
-    theme(legend.position="bottom")
-p9 <- ggsurv(afitage, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier estimates of\nall-cause survival by age group") +
-    theme(legend.position="bottom")
+p8 <- ggsurv(mfitage, cens.shape="|", cens.col = "black") + theme(legend.position="bottom") +
+    ggtitle("Kaplan-Meier estimates of\ncancer survival by age group")
+p9 <- ggsurv(afitage, cens.shape="|", cens.col = "black") + theme(legend.position="bottom") +
+    ggtitle("Kaplan-Meier estimates of\nall-cause survival by age group")
 grid.arrange(p8, p9, ncol=2)
 
 
