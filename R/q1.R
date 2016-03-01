ffe545c5082616cbbed03d7a240609c5fcd2a362
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-03-01 21:27:03 +0100

    Q1: simplified and commented the code

--- R/q1.R
+++ R/q1.R
@@ -1,57 +1,47 @@
 ## Purpose: To do the solution for Biostat III exercises in R
 ## Author: Annika Tillander, 2014-01-30
-## Edited: Andreas Karlsson, 2015-02-17
+## Edited: Andreas Karlsson, 2015-02-17, 2016-03-01
 ###############################################################################
 
 
-## Install needed packages only need to be done once
-## install.packages("foreign")
-## install.packages("survival")
-## install.packages("KMsurv")
-## install.packages("dplyr")
-## install.packages("ggplot2")
-## install.packages("GGally")
-## install.packages("gridExtra")
-## install.packages("RCurl")
 
 ###############################################################################
 ## Exercise 1b
 ###############################################################################
 
 ## @knitr loadDependecies
-require(foreign) #Needed to read data set from Stata
-require(survival)
-require(KMsurv)
-require(dplyr)
-require(ggplot2)
-require(GGally)
-require(gridExtra)
-require(RCurl)
+require(foreign)  # needed to read data set from Stata
+require(dplyr)    # for data manipulation
+require(KMsurv)   # for life-tables
+require(survival) # for Kaplan-Meier
 
 ## Get the data for exercise 1
 colon_sample <- read.dta("http://biostat3.net/download/colon_sample.dta")
 
 ## Create 0/1 outcome variable
-colon <- mutate(colon_sample,
-                death_cancer = ifelse( status == "Dead: cancer", 1, 0))
+colon <-colon_sample %>%
+    mutate(death_cancer = ifelse( status == "Dead: cancer", 1, 0))
 
-## @knitr eventsPerMonth
+## @knitr eventsPerYear
 colonByYear <- colon %>%
-    mutate(year = floor(surv_yy)) %>%
-    group_by(year) %>%
-    summarise(nevent = sum(death_cancer),
-              nlost = length(death_cancer)-sum(death_cancer))
+    mutate(year = floor(surv_yy)) %>%     # floor to whole years
+    group_by(year) %>%                    # summarise within each year
+    summarise(nevent = sum(death_cancer), # number of events
+              nlost = n() - nevent)       # number lost to follow-up
+
 
 ## @knitr lifeTable
-with(colonByYear, lifetab(c(year,tail(year,1)+1), 35, nlost, nevent))[,1:8]
+with(colonByYear,                           # using the colonByYear data
+     lifetab(tis = c(year, tail(year,1)+1), # should be one element longer for the intervals
+             ninit = nrow(colon),           # number of individuals at the start
+             nlost = nlost,                 # number lost for each interval
+             nevent = nevent)) %>%          # number of events for each interval
+    round(2)
 
 ## @knitr KaplanMeier
-mfit <- survfit(Surv(surv_mm, death_cancer) ~ 1, data = colon)
-summary(mfit)
-ggsurv(mfit) + ylab("S(t)") + xlab("Time since diagnosis in months") +
-    ggtitle("Kaplan−Meier estimates of cause−specific survival")
-
-## @knitr KaplanMeierNumberAtRisk
-ggkm <- eval(expr=parse(text=getURL("https://raw.githubusercontent.com/andreasakarlsson/BiostatIII/master/R/ggkm.R", .opts = list(ssl.verifypeer = FALSE))))
-sfit <- survfit(Surv(surv_mm, death_cancer) ~ sex, data = colon)
-ggkm(sfit, table=T, pval=F, timeby = 10)
+mfit <- survfit(Surv(surv_mm, death_cancer) ~ 1, data = colon) # make Kaplan-Meier estimates
+summary(mfit)                                                  # print Kaplan-Meier table
+plot(mfit,                                                     # plot Kaplan-Meier curve
+     ylab="S(t)",
+     xlab="Time since diagnosis in months",
+     main = "Kaplan−Meier estimates of cause−specific survival")
