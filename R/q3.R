cc394ddc59352ca8ef3a859c75436e4514b30831
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-03-08 17:17:57 +0100

    Q3: simplified the code

--- R/q3.R
+++ R/q3.R
@@ -1,36 +1,19 @@
 ## Purpose: To do the solution for Biostat III exercises in R
 ## Author: Annika Tillander, 2014-01-30
-## Edited: Andreas Karlsson, 2015-02-28
+## Edited: Andreas Karlsson, 2015-02-28, 2016-03-08
 ###############################################################################
-
-## Install needed packages only need to be done once
-## install.packages("survival")
-## install.packages("KMsurv")
-## install.packages("readstata13")
-## install.packages("dplyr")
-## install.packages("ggplot2")
-## install.packages("Gally")
-## install.packages("muhaz")
-## install.packages("gridExtra")
-## install.packages("RCurl")
-
 
 ###############################################################################
 ## Exercise 3
 ###############################################################################
 ## @knitr loadDependecies
-require(readstata13) #Needed to read data set from Stata 13
+require(foreign)  # needed to read data set from Stata
 require(survival) #for Surv and survfit
-require(muhaz) #for hazard estimates
-require(dplyr)
-require(ggplot2)
-require(GGally)
-require(gridExtra)
-require(RCurl)
-helperFunctions <- eval(expr=parse(text=getURL("https://raw.githubusercontent.com/andreasakarlsson/BiostatIII/master/R/helpFuncBioIII.R", .opts = list(ssl.verifypeer = FALSE))))
+require(muhaz)    #for hazard estimates
+require(dplyr)    # for data manipulation
 
 ## @knitr loadPreprocess
-melanoma_raw<- read.dta13("http://biostat3.net/download/melanoma.dta")
+melanoma_raw <- read.dta("http://biostat3.net/download/melanoma.dta")
 melanoma <- melanoma_raw %>%
     filter(stage=="Localised") %>%
     mutate(death_cancer = ifelse( status == "Dead: cancer", 1, 0),
@@ -38,13 +21,25 @@ melanoma <- melanoma_raw %>%
 
 ## @knitr a_survDiaDate
 mfityear8594 <- survfit(Surv(surv_mm, death_cancer==1) ~ year8594, data = melanoma)
-ggsurv(mfityear8594, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates")
 
+plot(mfityear8594, col = 1:2,
+     xlab = "Follow-up Time",
+     ylab = "Survival",
+     main = "Kaplan-Meier survival estimates")
+legend("topright", c("Diagnosed 75-84", "Diagnosed 85-94"), col=1:2, lty = 1)
 
 ## @knitr b_hazDiaDate
-ggplot(smoothHazard(melanoma,"year8594"), aes(x=Time, y=Hazard, colour= year8594)) +
-    geom_line() + ggtitle("Smoothed Hazard estimates")
+hazByGroup <- function(group){
+    with(subset(melanoma, group),
+         muhaz(times=surv_mm,
+               delta=death_cancer,
+               min.time = min(surv_mm),
+               max.time = max(surv_mm)))
+}
+
+plot(hazByGroup(melanoma$year8594 == "Diagnosed 75-84"), col=1, main="Smoothed hazard estimates")
+lines(hazByGroup(melanoma$year8594 == "Diagnosed 85-94"), col=2)
+legend("topright", c("Diagnosed 75-84", "Diagnosed 85-94"), col=1:2, lty = 1)
 
 ## @knitr c_testDiaDate
 ## Log-rank test for equality of survivor functions
@@ -60,15 +55,20 @@ melanoma %>%
     mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
-
 mfit_agegrp <- survfit(Surv(surv_mm, death_cancer==1) ~ agegrp, data = melanoma)
-ggsurv(mfit_agegrp, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates") + xlab("Months since diagnosis")
+plot(mfit_agegrp, col = 1:4,
+     xlab = "Months since diagnosis",
+     ylab = "Survival",
+     main = "Kaplan-Meier survival estimates")
+legend("bottomleft", c("0-44", "45-59", "60-74", "75+"), col=1:4, lty = 1)
 
 ## @knitr e_crudeRates1000_agegrp
-mfit_agegrp_year<- survfit(Surv(surv_mm/12, death_cancer==1) ~ agegrp, data = melanoma)
-ggsurv(mfit_agegrp_year, cens.shape="|", cens.col = "black") + 
-    ggtitle("Kaplan-Meier survival estimates") + xlab("Years since diagnosis") 
+mfit_agegrp_year <- survfit(Surv(surv_mm/12, death_cancer==1) ~ agegrp, data = melanoma)
+plot(mfit_agegrp_year, col = 1:4,
+     xlab = "Months since diagnosis",
+     ylab = "Survival",
+     main = "Kaplan-Meier survival estimates")
+legend("bottomleft", c("0-44", "45-59", "60-74", "75+"), col=1:4, lty = 1)
 
 melanoma %>%
     select(death_cancer, surv_mm, agegrp) %>%
@@ -77,15 +77,19 @@ melanoma %>%
     mutate(CI_low = Rate + qnorm(0.025) * Rate / sqrt(D),
            CI_high = Rate + qnorm(0.975) * Rate / sqrt(D))
 
-
 ## @knitr f_sexDiff
+par(mfrow=c(1, 2))
 mfit_sex <- survfit(Surv(surv_mm, death_cancer) ~ sex, data = melanoma)
-p1 <- ggsurv(mfit_sex, cens.shape="|", cens.col = "black") +
-    ggtitle("Kaplan-Meier survival estimates") + theme(legend.position="bottom")
-p2 <- ggplot(smoothHazard(melanoma,"sex"), aes(x=Time, y=Hazard, colour= sex)) +
-    geom_line() + ggtitle("Smoothed Hazard estimates") + theme(legend.position="bottom")
-grid.arrange(p1, p2, ncol=2)
 
+plot(mfit_sex, col = 1:2,
+     xlab = "Follow-up Time",
+     ylab = "Survival",
+     main = "Kaplan-Meier survival estimates")
+legend("topright", c("Male", "Female"), col=1:2, lty = 1)
+
+plot(hazByGroup(melanoma$sex == "Male"), col=1, main="Smoothed hazard estimates")
+lines(hazByGroup(melanoma$sex == "Female"), col=2)
+legend("topright", c("Male", "Female"), col=1:2, lty = 1)
 
 ## Log-rank test for equality of survivor functions
 survdiff(Surv(surv_mm, death_cancer==1) ~ sex, data=melanoma)
